42f7232255a87bdb019e056a3e37ce1e954dcd96
Piotr Wasilewski <piotr.m.wasilewski@gmail.com>
2014-08-28 19:14:06 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,188 @@
+AnimateScrollNavigation.js
+=============
+
+A Simple jQuery Plugin extending [animateScroll](https://github.com/ramswaroop/animatescroll.js) plugin with Single Page Scroll Capabilities
+
+
+Why bother?
+-------------------
+
+I needed jQuery plugin allowing me to nicely navigate around single page website and none I could find had all fetures I needed:
+
+* Smooth, continous scroll across sections
+* Ability to navigate to each and every section, also programatically
+* Possible menu integration, but I didin't want menu to be included as part of a package
+* Each section can have different hight
+* If section height is less then screen height - it can be adjusted
+* If section is higher then screen - we're cool
+* It had to support anchor links to sections, i.e. ```#section1```
+* It would support [History API](https://developer.mozilla.org/en-US/docs/Web/Guide/API/DOM/Manipulating_the_browser_history)
+
+This is why I bothered writing this plugin. If its features are what you are looking for - that's perfect! :)
+
+USAGE
+-------------------
+
+First of all - you'll need jQuery, so make sure to include it before plugin itself. Plugin works with both v1.x and 2.x versions. You can use CDN if you'd like.
+
+```html
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+```
+
+Secondly: This plugin uses animateScroll for handling all transitions, please also include it before animateScrollNavigation.js
+
+```html
+<script src="path/to/animatescroll.min.js"></script>
+```
+
+Now: animateScrollNavigation.js
+
+```html
+<script src="path/to/animate_scroll_navigation.min.js"></script>
+```
+
+animateScrollNavigation.js uses simple markup: one container, on which you call this plugin and an number of elements inside of it. You can set them with CSS selector, but it defaults to ```.section```. Every section has to have ```id``` with anchor name, i.e. ```id="section3"``` - this is good, because you can always fallback to regular anchor link behaviour.
+
+Let's say you'd like to have container ```#fullPage``` and standard sections containers ```.section```:
+
+```html
+<div id="fullpage">
+    <div class="section" id="section1">
+        section1
+    </div>
+    <div class="section" id="section2">
+        section2
+    </div>
+    <div class="section" id="section3">
+        section3
+    </div>
+    <div class="section" id="section4">
+        section4
+    </div>
+</div>
+```
+
+After that only thing left to do is init animateScrollNavigation.js with default options by calling:
+
+```js
+$(document).ready(function(){
+    $('#fullpage').animateScrollNavigation();
+});
+```
+
+Whole thing would look something like this:
+
+```html
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>animateScrollNavigation.js Demo</title>
+    </head>
+    <body>
+        <div id="fullpage">
+            <div class="section" id="section1">
+                section1
+            </div>
+            <div class="section" id="section2">
+                section2
+            </div>
+            <div class="section" id="section3">
+                section3
+            </div>
+            <div class="section" id="section4">
+                section4
+            </div>
+        </div>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+        <script src="../bower_components/animatescroll/animatescroll.min.js"></script>
+        <script src="../dist/animate_scroll_navigation.min.js"></script>
+        <script>
+            $(document).ready(function(){
+                $('#fullpage').animateScrollNavigation();
+            });
+        </script>
+    </body>
+</html>
+```
+
+Options
+-------------
+
+animateScrollNavigation.js accepts couple of parameters/startup options:
+
+```js
+$('#fullpage').animateScrollNavigation({
+        // selector for elements containing sections
+        element: '.section',
+
+        // determines if sections height should be calculated to match screen height
+        fullPage: true,
+
+        // disable animation (usefull for older browsers, etc.)
+        noAnimation: false,
+
+        // you can pass all animateScroll options, they will become global for all elements in navigation
+        animateScrollOptions: {}
+    });
+```
+
+API
+--------------
+
+There are few methods allowing to programatically use this navigation. Assuming you are using ```#fullpage``` as a selector, you can do some nce things, like:
+
+Go to ```next``` section with:
+
+```js
+$('#fullpage').animateScrollNavigation('next');
+```
+... or to ```previous``` section:
+
+```js
+$('#fullpage').animateScrollNavigation('previous');
+```
+
+In order to go to particular section (i.e. ````section2```), you can use this:
+
+```js
+$('#fullpage').animateScrollNavigation('goTo', 'section2');
+```
+
+... or you can use sections ID. They are numbered from 0 up:
+
+```js
+$('#fullpage').animateScrollNavigation('goTo', 1);
+```
+
+In order to get current section:
+
+```js
+$('#fullpage').animateScrollNavigation('getCurrent');
+
+// you'll get:
+//  {
+//      id: currentElementId,
+//      max: allElementsCount
+//  }
+```
+
+As this is based on ```animateScroll```, you can go to any element, even not included in Navigation markup by calling ```goToAny()``` with CSS selector as a first parameter. You can also pass any ```animateScroll``` options and optionally turn of any animation (defaults to false):
+
+```js
+$('#fullpage').animateScrollNavigation('goToAny', 'your-css-selector', animateScrollOptions, noAnimation);
+```
+
+
+Building / Minifing
+----------
+
+You can build minified version yourself, by simply using [Grunt](http://gruntjs.com) in project root.
+
+Contributing
+--------------
+
+1. Fork it
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am 'Add some feature'`)
+4. Push to the branch (`git push origin my-new-feature`)
+5. Create new Pull Request
---
+++ src/animate_scroll_navigation.js
@@ -0,0 +1,278 @@
+(function($){
+    'use strict';
+
+    // thi is me :)
+    var me = false;
+
+    // default options
+    var opts = {
+        // selector for elements containing sections
+        element: '.section',
+
+        // if sections height should be calculated to match screen height
+        fullPage: true,
+
+        // disable animation (usefull for older browsers, etc.)
+        noAnimation: false,
+
+        // you can pass all animateScroll options, they will become global for all elements
+        animateScrollOptions: {}
+    };
+
+    // dictionary od anchors
+    var elementHashes = [];
+
+    // default padding from top of the screen. usefull with fixed top menu bar.
+    var paddingTop = 0;
+
+    // elements library, indexed by anchor
+    // TODO: every element has 2 parameters: anchor and animateScroll options
+    var elements = {};
+
+    // current element ID
+    var currentElement = null;
+
+    // this flag disables onScroll() current element updating
+    var isAutoScroll = false;
+
+    // function returning an element
+    var showElement = function() {
+        // fire up animateScroll and go to selected element
+
+        opts.animateScrollOptions.onScrollStart = function() {
+          isAutoScroll = true;
+        };
+
+        opts.animateScrollOptions.onScrollEnd = function() {
+          isAutoScroll = false;
+        };
+
+        if (elementHashes[currentElement]) {
+          if (opts.noAnimation) {
+            $(document).scrollTop($("#" + elementHashes[currentElement]).offset().top - paddingTop);
+          } else {
+            $('#' + elementHashes[currentElement]).animatescroll(opts.animateScrollOptions);
+          }
+        }
+    };
+
+    var methods = {
+        init : function(options) {
+          me = this;
+          // checking if padding top is set
+          paddingTop = opts.animateScrollOptions.padding ? opts.animateScrollOptions.padding : 0;
+
+          // calculating full screen height of sections
+          if (opts.fullPage) {
+
+              methods.updateOffsets();
+
+              $(window).resize(function() {
+                  methods.updateOffsets();
+              });
+          }
+
+          methods.calculateOffsets();
+
+          // adding event on windows scroll, to check which element is currentlu active
+          var oldELement = 0;
+          $(window).on('scroll', function() {
+
+              // checking each element parameters
+              var len = elementHashes.length;
+              var windowHeight = $(window).height();
+              var currentScroll = $(window).scrollTop() + paddingTop;
+
+              for (var n = 0; n < len; n++) {
+
+                var tmpElement = $('#' + elementHashes[n]);
+                var currentElementOffset = tmpElement.offset().top;
+                var currentElementHeight = tmpElement.height();
+                var elementBottom = currentElementHeight + currentElementOffset;
+
+                // checking if elements header bottom (== site padding) is between elements offset and height
+                if (currentScroll > currentElementOffset && currentScroll < elementBottom) {
+
+                  // setting current element ID
+                  if (!isAutoScroll) {
+                    currentElement = n;
+                  }
+
+                  // checking if History API is available in the browser
+                  if (oldELement != currentElement) {
+                    if (history.pushState) {
+
+                        // pushing state to browser with History API
+                        history.pushState(null, null, '#' + elementHashes[currentElement]);
+
+                        $('[data-scroll!=""]').removeClass('active');
+                        $('[data-scroll="' + elementHashes[currentElement] + '"').addClass('active');
+                    }
+                    else {
+
+                        // no History API => old way
+                        location.hash = '#' + elementHashes[currentElement];
+                    }
+                  }
+
+                  oldELement = currentElement;
+                }
+              }
+            });
+
+            // small hack: we need to wait for calculations get finished (sections height/ offset)
+            setTimeout(function() {
+
+              // disabling animation, just for this action (only if it's not disable by default)
+              var noAnimationPresentState = opts.noAnimation;
+              opts.noAnimation = true;
+
+              methods.goTo(window.location.hash.substring(1));
+
+              // restoring default state from options
+              opts.noAnimation = noAnimationPresentState;
+            }, 200);
+        },
+
+        // showing next element
+        next : function() {
+            // only if next element won't be out of elements array
+            if ((currentElement + 1) <= elementHashes.length) {
+                currentElement += 1;
+            }
+            showElement();
+        },
+
+        // showing previous element
+        previous : function() {
+            // only if next element won't be out of elements array
+            if ((currentElement - 1) >= 0) {
+                currentElement -= 1;
+            }
+            showElement();
+        },
+
+        // go to particular element, either with ID or anchor text
+        goTo : function(elementId) {
+
+            // in case od number, we go to ID
+            if (typeof elementId === 'number') {
+                currentElement = elementId;
+            }
+            // in case of string, we assume anchor
+            else if (typeof elementId === 'string') {
+                currentElement = elementHashes.indexOf(elementId);
+            }
+
+            showElement();
+        },
+
+        // Wrapper for go to -> uses animationScroll options for goTo (i.e. noAnimation)
+        // but you can override them :)
+        // also: extra padding for non-animation can be set in animateScrollOptions = {padding: 120}
+        goToAny : function(elementSelector, animateScrollOptions, noAnimation) {
+
+            var noAnimationPresentState = opts.noAnimation;
+
+            var animateScrollOptionsTmp = (animateScrollOptions) ? animateScrollOptions : opts.animateScrollOptions;
+
+            if (noAnimation) {
+              opts.noAnimation = true;
+            }
+
+            var nonAnimationPadding = paddingTop;
+            if (animateScrollOptions.padding) {
+              nonAnimationPadding = animateScrollOptions.padding;
+            }
+
+            if (opts.noAnimation) {
+              $(document).scrollTop($(elementSelector).offset().top - nonAnimationPadding);
+            } else {
+              $(elementSelector).animatescroll(animateScrollOptionsTmp);
+            }
+
+            // restoring default state from options
+            opts.noAnimation = noAnimationPresentState;
+        },
+
+        onScrollEvent: function() {
+            // console.log()
+        },
+
+        getCurrent: function() {
+          return {
+            id: currentElement,
+            max: elementHashes.length
+          };
+        },
+
+        calculateOffsets: function() {
+          elementHashes = [];
+          elements = {};
+          $(me).find(opts.element).each(function(id, element) {
+
+              var anchor = $(element).attr('id');
+
+              // creating dictionary od anchors
+              elementHashes.push(anchor);
+
+              // setting current element ID, only for first element
+              if (!currentElement) {
+                  currentElement = 0;
+              }
+
+              // creating elements library, indexed by anchor
+              elements[anchor] = {
+                  anchor: anchor,
+                  height: $(element).height(),
+                  offsetTop: $(element).offset().top
+              };
+
+              // adding onClick event to all links with particular anchor url
+              $('a[href="#' + anchor + '"]').on('click', function(event) {
+                  event.preventDefault();
+                  methods.goTo(anchor);
+              });
+
+          });
+        },
+        updateOffsets: function() {
+          var minHeight = $(window).height() - paddingTop;
+          var currentOffset = paddingTop;
+
+          $(me).find(opts.element).each(function(id, element) {
+
+            var currentHeight = parseInt($(element).css('height'));
+            if (currentHeight < minHeight) {
+              currentHeight = minHeight;
+              $(element).css('min-height', minHeight);
+            }
+
+            $(element).offset({
+              top: currentOffset,
+              left: 0
+            });
+
+            currentOffset += currentHeight;
+
+          });
+        }
+    };
+
+    $.fn.animateScrollNavigation = function(methodOrOptions) {
+
+        if (methods[methodOrOptions]) {
+            return methods[methodOrOptions].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof methodOrOptions === 'object' || !methodOrOptions) {
+            // Default to "init"
+
+            // extending default options
+            opts = $.extend(opts, methodOrOptions);
+
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error('Method ' +  methodOrOptions + ' does not exist on jQuery.animateScrollNavigation');
+        }
+    };
+
+})(jQuery);
